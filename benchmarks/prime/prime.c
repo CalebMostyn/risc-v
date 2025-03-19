3f54bb66d0c64e964ad842214e1b117dde4b6237
CalebMostyn <mostyncr@miamioh.edu>
2025-03-19 02:10:09 -0400

    Added Prime Benchmark

---
+++ benchmarks/prime/prime.c
@@ -0,0 +1,13 @@
+int arr[50];
+int is_prime(int num) {
+    if (num < 2) return 0;
+    for (int i = 2; (i * i) <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+int main() {
+    for (int i = 0; i < 50; i++) {
+        arr[i] = is_prime(i);
+    }
+}
